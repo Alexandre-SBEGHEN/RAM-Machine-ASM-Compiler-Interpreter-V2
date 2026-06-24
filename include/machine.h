0eb75d310efcf05a6c12697577b83fae916122f3
Alexandre-SBEGHEN <alexandre.sbeghen-magnin@etu.univ-amu.fr>
2026-06-24 22:37:53 +0200

    feat(machine): Header machine.h
    Ajout des choses suivantes :
    - Alias des structures Reg et Mem ;
    - Structures RegStruct et MemStruct ;
    - Fonctions pour créer et supprimer strucures Reg/Mem.
    
    Documentation également faite.

---
+++ include/machine.h
@@ -0,0 +1,88 @@
+/**
+* @file machine.h
+ * @brief Machine RAM
+ *
+ * Allocation, suppression, gestion d'une
+ * machine RAM avec son registre.
+ *
+ * @author Alexandre SBEGHEN
+ * @date 2026-06-24
+ */
+
+#ifndef MACHINE_H
+#define MACHINE_H
+
+#include <stdint.h>
+
+/* --- Alias ---------------------------------------------------- */
+
+typedef struct RegStruct Reg;
+typedef struct MemStruct Mem;
+
+/* --- Structs -------------------------------------------------- */
+
+/**
+ * @brief Structure du registre.
+ * Contient son unique valeur de type int32_t.
+ */
+struct RegStruct {
+    int32_t val;
+};
+
+/**
+ * @brief Structure de la mémoire.
+ * Contient un tableau d'entiers de type int32_t.
+ */
+struct MemStruct {
+    int32_t* data;
+};
+
+/* --- Fonctions ------------------------------------------------ */
+
+/**
+ * @brief Crée dynamiquement une structure registre Reg.
+ *
+ * Valeur initialisée par défaut à 0.
+ *
+ * @return Pointeur vers la structure allouée.
+ *
+ * @note Penser à libérer la mémoire après utilisation.
+ * @see register_delete()
+ */
+Reg* register_create();
+
+/**
+ * @brief Libération de mémoire d'une structure Reg.
+ *
+ * Utilisation d'un double pointeur (adresse du pointeur
+ * en paramètre) pour libérer la mémoire puis mettre le
+ * pointeur à NULL.
+ *
+ * @param[in,out] reg Adresse du pointeur vers la structure.
+ */
+void register_delete(Reg** reg);
+
+/**
+ * @brief Crée dynamiquement une structure mémoire Mem.
+ *
+ * Valeurs initialisées à 0.
+ *
+ * @return Pointeur vers la structure allouée.
+ *
+ * @note Penser à libérer la mémoire après utilisation.
+ * @see memory_delete()
+ */
+Mem* memory_create();
+
+/**
+ * @brief Libération de mémoire d'une structure Mem.
+ *
+ * Utilisation d'un double pointeur (adresse du pointeur
+ * en paramètre) pour libérer la mémoire puis mettre le
+ * pointeur à NULL.
+ *
+ * @param[in,out] mem Adresse du pointeur vers la structure.
+ */
+void memory_delete(Mem** mem);
+
+#endif
