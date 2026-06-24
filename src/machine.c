08a0280152810e7574fbd54a6747b9b17473b38e
Alexandre-SBEGHEN <alexandre.sbeghen-magnin@etu.univ-amu.fr>
2026-06-24 23:06:19 +0200

    feat(machine): Code source machine.c
    Les fonctions n'ont pour l'instant aucun comportement / renvoient
    le minimum. Les tests sont donc encore invalides.

---
+++ src/machine.c
@@ -0,0 +1,25 @@
+/**
+ * @file machine.c
+ * @brief Implémentation de la machine RAM et ses opérations.
+ * @author Alexandre SBEGHEN
+ * @date 2026-06-24
+ */
+
+#include <stdlib.h>
+#include "machine.h"
+
+Reg* register_create() {
+    return NULL;
+}
+
+void register_delete(Reg* reg) {
+
+}
+
+Mem* memory_create() {
+    return NULL;
+}
+
+void memory_delete(Mem* mem) {
+
+}
