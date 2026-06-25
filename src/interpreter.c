0bf822c199796db7c5ad5753b75c1c46000a532d
Alexandre-SBEGHEN <alexandre.sbeghen-magnin@etu.univ-amu.fr>
2026-06-25 21:31:07 +0200

    feat(interpreter): Corps de la fonction program_interpret()
    Le test de la fonction passe.

--- src/interpreter.c
+++ src/interpreter.c
@@ -37,5 +37,68 @@ void program_delete(Program** prog) {
 
 /* Exécution d'un programme compilé */
 int program_interpret(const Program* prog, Machine* mac) {
-    return 0;
+    size_t inst_index = 0;
+
+    // Boucle du programme
+    while (1) {
+        // Index en dehors du programme (erreur 1)
+        if (inst_index >= prog->size)
+            return 1;
+
+        // Obtenir la paire opération / argument
+        int32_t op = prog->inst[inst_index].op;
+        int32_t arg = prog->inst[inst_index].arg;
+
+        // Action selon l'instruction
+        switch (op) {
+            // Ne rien faire
+            case 0:
+                break;
+            // LOAD #?
+            case 1:
+                ram_load_direct(mac, arg);
+                break;
+            // LOAD @?
+            case 2:
+                ram_load_from(mac, arg);
+            // STORE @?
+            case 3:
+                ram_store_to(mac, arg);
+                break;
+            // INCR
+            case 4:
+                ram_increment(mac);
+                break;
+            // DECR
+            case 5:
+                ram_decrement(mac);
+                break;
+            // JUMP ?
+            case 6:
+                // Gérer l'overflow (erreur 1)
+                if (arg < 0)
+                    return 1;
+                inst_index = arg;
+                continue;
+            // JZ ?
+            case 7:
+                // Gérer l'overflow (erreur 1)
+                if (arg < 0)
+                    return 1;
+                if (mac->reg->val <= 0) {
+                    inst_index = arg;
+                    continue;
+                }
+                break;
+            // HALT
+            case 8:
+                return 0;
+            // Instruction inconnue (erreur 2)
+            default:
+                return 2;
+        }
+
+        // Instruction suivante
+        ++inst_index;
+    }
 }
