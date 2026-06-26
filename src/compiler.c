cd4259e3d9561d6a517a637816d264952dedd02a
Alexandre-SBEGHEN <alexandre.sbeghen-magnin@etu.univ-amu.fr>
2026-06-26 12:19:07 +0200

    feat(compiler): Code source compiler.c, corps vide des fonctions
    Ajout également de la référence de compiler.c dans CMakeLists.txt.

---
+++ src/compiler.c
@@ -0,0 +1,25 @@
+/**
+ * @file interpreter.c
+ * @brief Implémentation du compilateur.
+ * @author Alexandre SBEGHEN
+ * @date 2026-06-26
+ */
+
+#include "compiler.h"
+#include "machine.h"
+#include "interpreter.h"
+
+/* Lecture de fichier dans une string */
+char* file_to_string(const char* filename) {
+    return NULL;
+}
+
+/* Découpage d'une string en tableau de lignes */
+char** string_to_array(const char* str) {
+    return NULL;
+}
+
+/* Nettoyage d'une ligne de code */
+void string_clean(char* str) {
+
+}
