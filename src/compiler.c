5c31d37adb4f02c9622a1b6de867b296761ae735
Alexandre-SBEGHEN <alexandre.sbeghen-magnin@etu.univ-amu.fr>
2026-06-26 15:17:50 +0200

    feat(compiler): file_to_string, string_to_array
    Tous les test passent

--- src/compiler.c
+++ src/compiler.c
@@ -6,17 +6,89 @@
  */
 
 #include "compiler.h"
-#include "machine.h"
-#include "interpreter.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /* Lecture de fichier dans une string */
 char* file_to_string(const char* filename) {
-    return NULL;
+    FILE* file = fopen(filename, "r");
+    if (file == NULL)
+        return NULL;
+
+    // Taille du fichier
+    fseek(file, 0, SEEK_END);
+    long size = ftell(file);
+    rewind(file);
+
+    // Allouer la string
+    char* str;
+    if ((str = malloc(size + 1)) == NULL) {
+        fclose(file);
+        return NULL;
+    }
+
+    fread(str, 1, size, file);
+    str[size] = '\0';
+
+    fclose(file);
+    return str;
 }
 
 /* Découpage d'une string en tableau de lignes */
 char** string_to_array(const char* str) {
-    return NULL;
+    char* copy; // Copie de str, pour strtok
+
+    if ((copy = strdup(str)) == NULL)
+        return NULL;
+    const char* sep = "\n";
+
+    // Calculer le nombre de lignes
+    size_t rows = 0;
+    size_t max_length = 0;
+    char* token = strtok(copy, sep);
+    while (token != NULL) {
+        ++rows;
+        if (strlen(token) > max_length)
+            max_length = strlen(token);
+        token = strtok(NULL, sep);
+    }
+
+    // Allouer le tableau de strings
+    char** array;
+    if ((array = malloc(rows * sizeof(char*))) == NULL) {
+        free(copy);
+        return NULL;
+    }
+    for (size_t i = 0; i < rows; ++i) {
+        // Libérer tous les précédents si échec de l'allocation de l'actuel
+        if ((array[i] = calloc(max_length + 1, sizeof(char))) == NULL) { // max_length ne compte pas \0
+            for (size_t j = 0; j < i; ++j)
+                free(array[j]);
+            free(array);
+            free(copy);
+            return NULL;
+        }
+    }
+
+    // On relit les token mais cette fois on les met dans le tableau
+    // Nouvelle copie de str
+    free(copy);
+    if ((copy = strdup(str)) == NULL) {
+        for (size_t i = 0; i < rows; ++i)
+            free(array[i]);
+        free(array);
+        return NULL;
+    }
+    // Copie dans le tableau
+    token = strtok(copy, sep);
+    for (size_t i = 0; i < rows; ++i) {
+        strcpy(array[i], token);
+        token = strtok(NULL, sep);
+    }
+
+    free(copy);
+    return array;
 }
 
 /* Nettoyage d'une ligne de code */
--- src/main.c
+++ src/main.c
@@ -6,11 +6,26 @@
  */
 
 #include <stdio.h>
+#include <string.h>
+
 #include "machine.h"
+#include "compiler.h"
 #include "interpreter.h"
 
 int main(void) {
+    const char* str = "Bonjour tout le monde !\nCeci est un paragraphe simple,\ntrès très simple...\nHihihi";
+    char** array = string_to_array(str);
 
+    char strtest_1[4][50] = {
+        "Bonjour tout le monde !",
+        "Ceci est un paragraphe simple,",
+        "très très simple...",
+        "Hihihi"
+    };
+
+    for (size_t i = 0; i < 4; ++i) {
+        printf("%s\t\t%s\t\t%d\n", array[i], strtest_1[i], strcmp(array[i], strtest_1[i]));
+    }
 
 
 
