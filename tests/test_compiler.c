046c07dfd6baefd242f859c72c1981e2efc85052
Alexandre-SBEGHEN <alexandre.sbeghen-magnin@etu.univ-amu.fr>
2026-06-26 12:31:21 +0200

    tests(compiler): Implémentation du test_compiler dans le projet
    Ajout dans le CMakeLists.txt.
    
    A noter que le fichier de test est encore vide.

---
+++ tests/test_compiler.c
@@ -0,0 +1,14 @@
+/**
+ * @file test_compiler.c
+ * @brief Test des implémentations de compiler.h / compiler.c
+ * @author Alexandre SBEGHEN
+ * @date 2026-06-25
+*/
+
+#include <assert.h>
+#include <string.h>
+#include "compiler.h"
+
+int main() {
+
+}
